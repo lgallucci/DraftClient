1a305bd7e96ee15167fa6f3a31a44ff883dd8580
lgallucci-work <lou.gallucci86@gmail.com>
2015-06-30 16:40:41 -0500

    Add schedule to player flyout

--- DraftClient/View/PlayerFlyout.xaml.cs
+++ DraftClient/View/PlayerFlyout.xaml.cs
@@ -1,5 +1,7 @@
 ﻿namespace DraftClient.View
 {
+    using System.Windows;
+    using MahApps.Metro;
     using MahApps.Metro.Controls;
 
     /// <summary>
@@ -10,6 +12,11 @@
         public PlayerFlyout()
         {
             InitializeComponent();
+        }
+
+        public double LogoOpacity
+        {
+            get { return (ThemeManager.DetectAppStyle(Application.Current).Item1.Name == "BaseDark") ? .45 : .15; }
         }
     }
 }
