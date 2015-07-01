089685ba213839232f6b61ecf6f208cc99365f3e
lgallucci-work <lou.gallucci86@gmail.com>
2015-07-01 14:50:05 -0500

    More information on the player flyout

---
+++ DraftClient/Converters/AgeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace DraftClient.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class AgeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string))
+            {
+                throw new InvalidOperationException("The target must be a string");
+            }
+
+            return (int) value > 0 ? value : "N/A";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
